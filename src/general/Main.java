21d6b26b861e671aa3082ab2981be04e6dc6da37
H1ghBre4k3r <H1ghBre4k3r@users.noreply.github.com>
2018-02-26 14:47:04 +0100

    .

--- src/general/Main.java
+++ src/general/Main.java
@@ -13,12 +13,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
-<<<<<<< HEAD
-		DesktopFrame frame = new DesktopFrame(WIDTH, HEIGHT, TITLE);
 
-=======
-		
->>>>>>> 0c2f45c028cedf76337e0d7bf1d292bd7db57bfc
+		DesktopFrame frame = new DesktopFrame(WIDTH, HEIGHT, TITLE);
 	}
 
 }
